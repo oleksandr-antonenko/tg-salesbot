9c9813eb4c56ab2ff26f593eb09ffae583dded7c
Oleksandr Antonenko <antonenko.web.dev@gmail.com>
2025-08-01 17:28:11 +0300

    ukrainian

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -210,8 +210,8 @@ export class TelegramService implements OnModuleInit, OnModuleDestroy {
               [
                 { text: '🇺🇸 English', callback_data: 'lang_en' },
                 { text: '🇺🇦 Українська', callback_data: 'lang_uk' },
+                { text: 'Русский', callback_data: 'lang_ru' },
               ],
-              [{ text: 'Русский', callback_data: 'lang_ru' }],
             ],
           },
         });
